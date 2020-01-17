1642003c890f6c610403a5021790826b89a80f72
bekalouc <bekalouc@sheridancollege.ca>
2020-01-17 12:02:35 -0500

    initial commit of source code

---
+++ src/student/Student.java
@@ -0,0 +1,64 @@
+package student;
+
+/**
+ * A class to model students.
+ * Students can study. They have a 
+ * name and id
+ *
+ * @author Kaden Beka Loucks
+ */
+public class Student {
+   
+    private String name;
+    private String studentID;
+    
+    /**
+     * a constructor that takes in both parameters 
+     * @param givenName - the students name
+     * @param givenID - the students ID
+     */
+    
+    public Student(String givenName, String givenID){
+        name = givenName;
+        studentID = givenID;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param givenName the name to set
+     */
+    public void setName(String givenName) {
+        name = givenName;
+    }
+
+    /**
+     * @return the studentID
+     */
+    public String getStudentID() {
+        return studentID;
+    }
+
+    /**
+     * @param studentID the studentID to set
+     */
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+    
+    /**
+     * a method that returns true if a student 
+     * studied and false otherwise
+     * @return whether or not the student study
+     */
+    
+    public boolean didstudy(){
+        return true;
+    }// end boolean
+
+}// end class
